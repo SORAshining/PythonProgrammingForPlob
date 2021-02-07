7bb4fb6ef1327969b4440b877017f4f41663bd6c
Lain <41903769+SORAshining@users.noreply.github.com>
2021-02-08 03:40:54 +0900

    first commit

---
+++ FlipYourCap.py
@@ -0,0 +1,29 @@
+cap1 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'B', 'F']
+
+def pleaseConform(caps: 'list') -> 'strings':
+    start = forward = backward = 0
+    intervals = []
+    for i in range(1, len(caps)):
+        if caps[start] != caps[i]:
+            intervals.append((start, i-1, caps[start]))
+            if caps[start] == 'F':
+                forward += 1
+            else:
+                backward += 1
+            start = i
+    intervals.append((start, len(caps) - 1, caps[start]))
+    if caps[start] == 'F':
+        forward += 1
+    else:
+        backward += 1
+    if forward < backward:
+        flip = 'F'
+    else:
+        flip = 'B'
+    for t in intervals:
+        if (t[0] == t[1]) and (t[2] == flip):
+            print('Person at position', t[0], 'flip your caps!')
+        elif t[2] == flip:
+            print('People in positions', t[0], 'through', t[1], 'flip your caps!')
+
+pleaseConform(cap1)
